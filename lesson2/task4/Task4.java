7fd435c1b7d7203b18e5404d1fe365a61a4d8fe5
Aleksei Grinko <als.grinko@yandex.ru>
2023-06-14 09:42:15 +0400

    completed the task4

--- lesson2/task4/Task4.java
+++ lesson2/task4/Task4.java
@@ -1,7 +1,5 @@
 package javaseminar.lesson2.task4;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
@@ -10,14 +8,57 @@ import java.util.logging.SimpleFormatter;
 
 public class Task4 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         Task4 task4 = new Task4();
+        task4.getTask3(task4);
+    }
+
+    void getTask3(Task4 task4) { // реалиация калькулятора с логированием
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое число: ");
+        int number1 = scanner.nextInt();
+        System.out.println("Введите второе число: ");
+        int number2 = scanner.nextInt();
+        System.out.println("Введите знак арифметического действия (+ - * /): ");
+        String sign = scanner.next();
+        task4.calculation(number1, number2, sign);
+    }
+
+    private void calculation (int number1, int number2, String sign) {
+        Logger logger = Logger.getLogger(Task4.class.getName());
+        try {
+            FileHandler fileHandler = new FileHandler("D:\\GB\\HW\\javaseminar\\lesson2\\task4\\log.txt", 10000, 1, true);
+            logger.addHandler(fileHandler);
+            SimpleFormatter sFormatter = new SimpleFormatter();
+            fileHandler.setFormatter(sFormatter);
+        } catch (Exception e) {
+            logger.log(Level.WARNING, e.getMessage());
+        }
+        if (sign.equals("+")) {
+            logger.log(Level.INFO, number1 + sign + number2 + "=" + (number1 + number2));
+            System.out.println(number1 + sign + number2 + "=" + (number1 + number2));
+        } else if (sign.equals("-")) {
+            logger.log(Level.INFO, number1 + sign + number2 + "=" + (number1 - number2));
+            System.out.println(number1 + sign + number2 + "=" + (number1 - number2));
+        } else if (sign.equals("*")) {
+            logger.log(Level.INFO, number1 + sign + number2 + "=" + (number1 * number2));
+            System.out.println(number1 + sign + number2 + "=" + (number1 * number2));
+        } else if (sign.equals("/")) {
+            logger.log(Level.INFO, number1 + sign + number2 + "=" + ((double)number1 / number2));
+            System.out.println(number1 + sign + number2 + "=" + ((double)number1 / number2));
+        } else {
+            logger.log(Level.INFO, "Такая операция не поддерживается");
+            System.out.println("Такая операция не поддерживается");
+        }
+    }
+
+    void getTask4(Task4 task4) { //перепутал с калькулятором, реализовал условие задачи для дополнительного задания предыдущего урока
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Задайте уравнение вида q + w = e, где q, w, e >= 0:");
         String line = scanner.nextLine();
         System.out.println(task4.getResult(line));
     }
 
-    String getResult(String line) {
+    private String getResult(String line) {
         String message = "Нет доступных вариантов решения уровнения";
         Logger logger = Logger.getLogger(Task4.class.getName());
         try {
